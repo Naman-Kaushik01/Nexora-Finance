3ece8e79e62c0942e5fe0c34307599ff5d55989d
Naman Kaushik <kaushiknaman68@gmail.com>
2026-03-05 03:09:16 +0530

    connected to database

---
+++ src/main/java/com/Nexora/NexoraFinance/NexoraFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.Nexora.NexoraFinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexoraFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexoraFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Nexora/NexoraFinance/NexoraFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nexora.NexoraFinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexoraFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
