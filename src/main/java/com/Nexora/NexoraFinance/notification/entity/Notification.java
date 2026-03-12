94f3dc3bbfecd5576ad81b3a0153f2af6b36cfcb
Naman Kaushik <kaushiknaman68@gmail.com>
2026-03-13 01:07:30 +0530

    created Notification entity

---
+++ src/main/java/com/Nexora/NexoraFinance/notification/entity/Notification.java
@@ -0,0 +1,38 @@
+package com.Nexora.NexoraFinance.notification.entity;
+
+import com.Nexora.NexoraFinance.auth_users.entity.User;
+import com.Nexora.NexoraFinance.enums.NotificationType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String subject;
+    private String recipient;
+
+    private String body;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType type; // EMAIL , SMS , PUSH
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private final LocalDateTime createdAt = LocalDateTime.now();
+
+}
