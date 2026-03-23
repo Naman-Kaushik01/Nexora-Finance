65011f61e31813c6aeecbdc58bf0303fd8c10566
Naman Kaushik <kaushiknaman68@gmail.com>
2026-03-23 16:36:13 +0530

    created AuthFilter

---
+++ src/main/java/com/Nexora/NexoraFinance/security/AuthFilter.java
@@ -0,0 +1,41 @@
+package com.Nexora.NexoraFinance.security;
+
+import com.Nexora.NexoraFinance.exceptions.CustomAuthenticationEntryPoint;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class AuthFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = getTokenFromRequest(request);
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        String tokenWithBearer = request.getHeader("Authorization");
+        if (tokenWithBearer != null && tokenWithBearer.startsWith("Bearer ")) {
+            return tokenWithBearer.substring(7);
+        }
+        return null;
+
+    }
+}
