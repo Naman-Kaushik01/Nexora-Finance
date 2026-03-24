116b417376b8f5f0aa6c3dad3b467ba8f0bb640e
Naman Kaushik <kaushiknaman68@gmail.com>
2026-03-25 01:38:25 +0530

    send mail method created

--- src/main/java/com/Nexora/NexoraFinance/notification/services/NotificationServiceImpl.java
+++ src/main/java/com/Nexora/NexoraFinance/notification/services/NotificationServiceImpl.java
@@ -3,12 +3,18 @@ package com.Nexora.NexoraFinance.notification.services;
 import com.Nexora.NexoraFinance.auth_users.entity.User;
 import com.Nexora.NexoraFinance.notification.dtos.NotificationDTO;
 import com.Nexora.NexoraFinance.notification.repo.NotificationRepo;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.nio.charset.StandardCharsets;
 
 @Service
 @Slf4j
@@ -23,5 +29,30 @@ public class NotificationServiceImpl implements NotificationService {
     @Async
     public void sendEmail(NotificationDTO notificationDTO, User user) {
 
+        try{
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(
+                    mimeMessage,
+                    MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                    StandardCharsets.UTF_8.name()
+            );
+            helper.setTo(notificationDTO.getRecipient());
+            helper.setSubject(notificationDTO.getSubject());
+
+            if(notificationDTO.getTemplateName() != null) {
+                Context context = new Context();
+                context.setVariables(notificationDTO.getTemplateVariables());
+                String htmlContent = templateEngine.process(notificationDTO.getTemplateName(), context);
+                helper.setText(htmlContent, true);
+            }else  {
+                helper.setText(notificationDTO.getBody(), true);
+            }
+
+            //Save it to our database table
+            mailSender.send(mimeMessage);
+
+        }catch (MessagingException e){
+            log.error(e.getMessage());
+        }
     }
 }
