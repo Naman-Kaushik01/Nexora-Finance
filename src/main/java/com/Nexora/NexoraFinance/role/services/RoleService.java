aea65ba141c77af1e3c3f1426647bace24f55af3
Naman Kaushik <kaushiknaman68@gmail.com>
2026-03-25 20:04:52 +0530

    created RoleService

---
+++ src/main/java/com/Nexora/NexoraFinance/role/services/RoleService.java
@@ -0,0 +1,17 @@
+package com.Nexora.NexoraFinance.role.services;
+
+import com.Nexora.NexoraFinance.res.Response;
+import com.Nexora.NexoraFinance.role.entity.Role;
+
+import java.util.List;
+
+public interface RoleService {
+    Response<Role> createRole(Role role);
+
+    Response<Role> updateRole(Role role);
+
+    Response<List<Role>> getAllRoles(Role role);
+
+    Response<?> deleteRole(Long id);
+
+}
