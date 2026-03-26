6964cdfa65ffd35e7adacb9444de7503758228bd
Naman Kaushik <kaushiknaman68@gmail.com>
2026-03-26 20:19:34 +0530

    created AuthController

---
+++ src/main/java/com/Nexora/NexoraFinance/auth_users/controller/AuthController.java
@@ -0,0 +1,45 @@
+package com.Nexora.NexoraFinance.auth_users.controller;
+
+import com.Nexora.NexoraFinance.auth_users.dtos.LoginRequest;
+import com.Nexora.NexoraFinance.auth_users.dtos.LoginResponse;
+import com.Nexora.NexoraFinance.auth_users.dtos.RegistrationRequest;
+import com.Nexora.NexoraFinance.auth_users.dtos.ResetPasswordRequest;
+import com.Nexora.NexoraFinance.auth_users.services.AuthService;
+import com.Nexora.NexoraFinance.res.Response;
+import com.Nexora.NexoraFinance.role.entity.Role;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response<String>> createRole(@RequestBody @Valid RegistrationRequest registrationRequest) {
+        return ResponseEntity.ok(authService.register(registrationRequest));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response<LoginResponse>> login(@RequestBody @Valid LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.login(loginRequest));
+    }
+
+    @PostMapping("/forget-password")
+    public ResponseEntity<Response<?>> forgetPassword(@RequestBody ResetPasswordRequest resetPasswordRequest) {
+        return ResponseEntity.ok(authService.forgetPassword(resetPasswordRequest.getEmail()));
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<Response<?>> resetPassword(@RequestBody ResetPasswordRequest resetPasswordRequest) {
+        return ResponseEntity.ok(authService.updatePasswordViaResetCode(resetPasswordRequest));
+    }
+
+}
