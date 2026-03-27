0e614b9a4fd49cfc71f8e4492a71978ecec8d247
Naman Kaushik <kaushiknaman68@gmail.com>
2026-03-27 20:24:42 +0530

    Implementing UserService

---
+++ src/main/java/com/Nexora/NexoraFinance/auth_users/services/impl/UserServiceImpl.java
@@ -0,0 +1,64 @@
+package com.Nexora.NexoraFinance.auth_users.services.impl;
+
+import com.Nexora.NexoraFinance.auth_users.dtos.UpdatePasswordRequest;
+import com.Nexora.NexoraFinance.auth_users.dtos.UserDTO;
+import com.Nexora.NexoraFinance.auth_users.entity.User;
+import com.Nexora.NexoraFinance.auth_users.repo.UserRepo;
+import com.Nexora.NexoraFinance.auth_users.services.UserService;
+import com.Nexora.NexoraFinance.exceptions.NotFoundException;
+import com.Nexora.NexoraFinance.notification.services.NotificationService;
+import com.Nexora.NexoraFinance.res.Response;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private final UserRepo userRepo;
+    private final NotificationService notificationService;
+    private final PasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public User getCurrentLoggedInUser() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication == null) {
+           throw new NotFoundException("User is not authenticated");
+        }
+       String email = authentication.getName();
+        return userRepo.findByEmail(email).orElseThrow(
+                () -> new NotFoundException("User not found with email: " + email)
+        );
+    }
+
+    @Override
+    public Response<UserDTO> getMyProfile() {
+        return null;
+    }
+
+    @Override
+    public Response<Page<UserDTO>> getAllUsers(int page, int size) {
+        return null;
+    }
+
+    @Override
+    public Response<?> updatePassword(UpdatePasswordRequest updatePasswordRequest) {
+        return null;
+    }
+
+    @Override
+    public Response<?> uploadProfilePicture(MultipartFile file) {
+        return null;
+    }
+}
