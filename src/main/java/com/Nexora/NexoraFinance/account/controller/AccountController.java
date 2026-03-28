cb8180e83c0c9c7488fb1cd3b5be3609c34d99a0
Naman Kaushik <kaushiknaman68@gmail.com>
2026-03-28 21:23:21 +0530

    created AccountController

---
+++ src/main/java/com/Nexora/NexoraFinance/account/controller/AccountController.java
@@ -0,0 +1,28 @@
+package com.Nexora.NexoraFinance.account.controller;
+
+import com.Nexora.NexoraFinance.account.services.AccountService;
+import com.Nexora.NexoraFinance.res.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.Files;
+
+@RestController
+@RequestMapping("/api/accounts")
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @GetMapping("/me")
+    public ResponseEntity<Response<?>> getMyAccounts(){
+        return ResponseEntity.ok(accountService.getMyAccounts());
+    }
+
+    @DeleteMapping("/close/{accountNumber}")
+    public ResponseEntity<Response<?>> closeAccount(@PathVariable String accountNumber){
+        return ResponseEntity.ok(accountService.closeAccount(accountNumber));
+    }
+
+}
